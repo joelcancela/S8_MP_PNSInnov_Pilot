be8800471e7c57721d6ca9e234fe24486bbcb53c
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2018-05-30 10:00:50 +0200

    Trying to fix redirection

--- src/main/java/unice/polytech/si4/pnsinnov/teamm/drive/GDriveOAuth.java
+++ src/main/java/unice/polytech/si4/pnsinnov/teamm/drive/GDriveOAuth.java
@@ -2,13 +2,18 @@ package unice.polytech.si4.pnsinnov.teamm.drive;
 
 import unice.polytech.si4.pnsinnov.teamm.api.Login;
 
-import javax.ws.rs.QueryParam;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static unice.polytech.si4.pnsinnov.teamm.api.Login.googleDrive;
 
 /**
  * Class GDriveOAuth
@@ -20,7 +25,9 @@ public class GDriveOAuth {
 	private static final Logger logger = Logger.getLogger(GDriveOAuth.class.getName());
 
 	@GET
-	public Response receiveCodeGDrive(@QueryParam("code") String code) {
+	public void receiveCodeGDrive(@Context HttpServletRequest request,
+	                              @Context HttpServletResponse response,
+	                              @QueryParam("code") String code) throws ServletException, IOException {
 		GDriveSession gDriveSession = Login.gDriveSession;
 		if (gDriveSession.credential == null) {
 			try {
@@ -29,7 +36,11 @@ public class GDriveOAuth {
 				logger.log(Level.INFO, e.getMessage());
 			}
 		}
-		logger.log(Level.SEVERE,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> GDRIVE IS CALLED");
-		return Response.status(200).build();
+
+		googleDrive.initialize();
+		googleDrive.subscribeToChanges();
+
+		request.setAttribute("list", googleDrive.getFilesList());
+		request.getRequestDispatcher("/PrivateMemo/gdrive-list.jsp").forward(request, response);
 	}
 }
