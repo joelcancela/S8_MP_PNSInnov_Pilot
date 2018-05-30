6e1316eed6018086cf317b063ee9358a77d8510d
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2018-05-30 09:28:51 +0200

    GDriveSession Debugging

--- src/main/java/unice/polytech/si4/pnsinnov/teamm/drive/GDriveSession.java
+++ src/main/java/unice/polytech/si4/pnsinnov/teamm/drive/GDriveSession.java
@@ -4,6 +4,7 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import unice.polytech.si4.pnsinnov.teamm.config.ConfigurationLoader;
 
 import java.io.IOException;
@@ -28,8 +29,16 @@ public class GDriveSession {
 
 	public void setCredential(String code) throws IOException {//NOT SURE TODO:
 		logger.log(Level.INFO, "AuthCode: " + code);
-		TokenResponse response = flow.newTokenRequest(code).setRedirectUri(redirectURL).execute();
-		credential = this.flow.createAndStoreCredential(response, userID);
+		logger.log(Level.INFO, "Flow: " + flow.toString());
+		GoogleAuthorizationCodeTokenRequest tokenRequest = flow.newTokenRequest(code);
+		logger.log(Level.INFO, "TOKEN RQST: " + tokenRequest);
+		logger.log(Level.INFO, "REDIRECT URL: " + redirectURL);
+		tokenRequest = tokenRequest.setRedirectUri(redirectURL);
+		logger.log(Level.INFO, "TOKEN RQST: " + tokenRequest);
+		TokenResponse response = tokenRequest.execute();
+		logger.log(Level.INFO, "TOKEN RESPONSE: " + response);
+		logger.log(Level.INFO, "USERID: " + userID);
+		credential = this.flow.createAndStoreCredential(response, userID);//FIXME: NPE ON RESPONSE
 	}
 
 	public void setFlow(GoogleAuthorizationCodeFlow flow) {
