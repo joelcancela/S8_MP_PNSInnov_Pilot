74946160623b2641347b3bcf28a25303438a2fed
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-06-05 01:06:28 +0200

    PNSM-33 List files according user authentification cookie

--- src/main/java/unice/polytech/si4/pnsinnov/teamm/api/ListingPage.java
+++ src/main/java/unice/polytech/si4/pnsinnov/teamm/api/ListingPage.java
@@ -1,5 +1,8 @@
 package unice.polytech.si4.pnsinnov.teamm.api;
 
+import unice.polytech.si4.pnsinnov.teamm.drive.GDrive;
+import unice.polytech.si4.pnsinnov.teamm.drive.GDriveSession;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -19,16 +22,26 @@ public class ListingPage {
 	private static final Logger logger = Logger.getLogger(ListingPage.class.getName());
 	@GET
 	public void getPage(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+		String userID = null;
+
 		Cookie cookies[] = request.getCookies();
 		for (Cookie c : cookies) {
 			logger.log(Level.INFO, "FOUND COOKIE : " + c.getName() + " Valued : " + c.getValue());
+			if (c.getName().equals("userID")) userID = c.getValue();
 		}
-		//request.setAttribute("posts", "UN MOT");
-		try {
-			request.getRequestDispatcher("/gdrive-list.jsp").forward(request, response);
-		} catch (ServletException | IOException e) {
-			e.printStackTrace();
-			logger.log(Level.SEVERE, "Imposssible to dispatch gdrive-list.jsp");
+
+		if (userID != null) {
+			GDriveSession session = Login.getDriveSessions(userID);
+			try {
+				request.setAttribute("ownFile", GDrive.getGDrive().classifyFiles(session));
+				request.getRequestDispatcher("/gdrive-list.jsp").forward(request, response);
+			} catch (ServletException | IOException e) {
+				e.printStackTrace();
+				logger.log(Level.SEVERE, "Imposssible to dispatch gdrive-list.jsp");
+			}
+		} else {
+			// TODO : HANDLE THIS CASE PROPERLY
+			throw  new RuntimeException("You must be connected to list your files");
 		}
 	}
 }
