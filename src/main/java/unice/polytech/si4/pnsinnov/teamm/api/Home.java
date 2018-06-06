8b728629360cf1b73c30d8ad403298fb6d610b1d
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2018-06-06 14:37:33 +0200

    Example Viewable

---
+++ src/main/java/unice/polytech/si4/pnsinnov/teamm/api/Home.java
@@ -0,0 +1,24 @@
+package unice.polytech.si4.pnsinnov.teamm.api;
+
+import org.glassfish.jersey.server.mvc.Viewable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Class Home
+ *
+ * @author Joël CANCELA VAZ
+ */
+@Path("/")
+public class Home {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Response index() {
+		return Response.ok(new Viewable("/index.jsp", null)).build();
+	}
+}
